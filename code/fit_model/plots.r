20ec931af437e54b62a3c2aab57e1d952d50809f
Bruna Amaral <amaral.bruuna@gmail.com>
2024-04-11 11:04:06 -0400

    make things easy to plot from hpc!

--- code/fit_model/plots.r
+++ code/fit_model/plots.r
@@ -1,25 +1,61 @@
 library(tidyverse)
 library(MCMCvis)
+library(rjags)
 
-samples_jags <- read_rds("data/model_res/jags_res_AMGO.rds")
-
-MCMCsummary(samples_jags,
-             params = c("mu.beta0","beta1", "beta2","beta3",
-                        "mu.alpha0","alpha1","alpha2","alpha3",
-                        "scales_beta1","scales_beta2"),
-                        round = 2)
+samples_jags <- read_rds("data/model_res/jags_res_NOCA2d.rds")
 
 MCMCsummary(samples_jags,
-             params = c("mu.beta0","beta",
-                        "mu.alpha0","alpha",
-                        "scales_beta1","scales_beta2"),
+            round = 2)
+
+MCMCsummary(samples_jags,
+            params = c("mu.beta0","beta1", "beta2","beta3",
+                       "mu.alpha0","alpha1","alpha2","alpha3",
+                      "scales_beta1","scales_beta2"),
+            round = 2)
+
+MCMCsummary(samples_jags,
+            params = c("mu.beta0","beta",
+                       "mu.alpha0","alpha",
+                      "scales_beta1","scales_beta2"),
                         round = 2)
- MCMCtrace(samples_jags,
+## trace ----------------------
+MCMCtrace(samples_jags,
            #params = params[c(2,4,5,7)],
            ind = TRUE,
-           pdf = FALSE)
+           pdf = TRUE)
 
- par(mfrow = c(1,1))
- MCMCplot(samples_jags,
-          #params = params[c(2,4,5,7)],
+MCMCtrace(samples_jags,
+           params = c("mu.beta0","beta1", "beta2","beta3",
+                    "mu.alpha0","alpha1","alpha2","alpha3",
+                    "scales_beta1","scales_beta2"),
+           ind = TRUE,
+           pdf = TRUE)
+
+MCMCtrace(samples_jags,
+           params = c("mu.beta0","beta",
+                    "mu.alpha0","alpha",
+                    "scales_beta1","scales_beta2"),
+           ind = TRUE,
+           pdf = TRUE)
+
+# par estimates ----------------------------------
+par(mfrow = c(1,1))
+MCMCplot(samples_jags,
+          #params = c("mu.beta0","beta1", "beta2","beta3",
+              #          "mu.alpha0","alpha1","alpha2","alpha3",
+               #         "scales_beta1","scales_beta2"),
           ref_ovl = TRUE)
+MCMCplot(samples_jags,
+         params = c("mu.beta0","beta", 
+                    "mu.alpha0","alpha",
+                    "scales_beta1","scales_beta2"),
+         ref_ovl = TRUE)
+
+MCMCplot(samples_jags,
+         params = c("mu.beta0","beta1", "beta2","beta3",
+                    "mu.alpha0","alpha1","alpha2","alpha3",
+                    "scales_beta1","scales_beta2"),
+         ref_ovl = TRUE)
+         
+
+summary(samples_jags)
