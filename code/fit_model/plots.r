7388b3e176b3a0cf6bec5a843ca9ed6f57e55e92
Bruna Amaral <amaral.bruuna@gmail.com>
2024-04-15 12:48:04 -0400

    code to run with any speecies

--- code/fit_model/plots.r
+++ code/fit_model/plots.r
@@ -2,16 +2,18 @@ library(tidyverse)
 library(MCMCvis)
 library(rjags)
 
-samples_jags <- read_rds("data/model_res/jags_res_SOSP2d.rds")
+sps <- "COYE"
+
+samples_jags <- read_rds(glue("data/model_res/jags_res_{sps}2d.rds"))
 
 MCMCsummary(samples_jags,
             round = 2)
 
-MCMCsummary(samples_jags,
-            params = c("mu.beta0","beta1", "beta2","beta3",
-                       "mu.alpha0","alpha1","alpha2","alpha3",
-                      "scales_beta1","scales_beta2"),
-            round = 2)
+#MCMCsummary(samples_jags,
+#            params = c("mu.beta0","beta1", "beta2","beta3",
+#                       "mu.alpha0","alpha1","alpha2","alpha3",
+#                      "scales_beta1","scales_beta2"),
+#            round = 2)
 
 MCMCsummary(samples_jags,
             params = c("mu.beta0","beta",
@@ -19,24 +21,28 @@ MCMCsummary(samples_jags,
                       "scales_beta1","scales_beta2"),
                         round = 2)
 ## trace ----------------------
-MCMCtrace(samples_jags,
-           #params = params[c(2,4,5,7)],
-           ind = TRUE,
-           pdf = TRUE)
+#MCMCtrace(samples_jags,
+#           #params = params[c(2,4,5,7)],
+#           ind = TRUE,
+#           pdf = TRUE)
+
+#MCMCtrace(samples_jags,
+#           params = c("mu.beta0","beta1", "beta2","beta3",
+#                    "mu.alpha0","alpha1","alpha2","alpha3",
+#                    "scales_beta1","scales_beta2"),
+#           ind = TRUE,
+#           pdf = TRUE)
 
 MCMCtrace(samples_jags,
-           params = c("mu.beta0","beta1", "beta2","beta3",
-                    "mu.alpha0","alpha1","alpha2","alpha3",
-                    "scales_beta1","scales_beta2"),
-           ind = TRUE,
-           pdf = TRUE)
-
-MCMCtrace(samples_jags,
-           params = c("mu.beta0","beta",
-                    "mu.alpha0","alpha",
-                    "scales_beta1","scales_beta2"),
-           ind = TRUE,
-           pdf = TRUE)
+          params = c("mu.beta0","beta",
+                   "mu.alpha0","alpha",
+                   "scales_beta1","scales_beta2"),
+          ind = TRUE,
+          pdf = TRUE,
+          filename = glue("{sps}_2b_trace_{nrow(samples_jags[[1]])}its"),
+          exact = TRUE,
+          Rhat = TRUE,
+          n.eff = TRUE)
 
 # par estimates ----------------------------------
 par(mfrow = c(1,1))
@@ -51,11 +57,11 @@ MCMCplot(samples_jags,
                     "scales_beta1","scales_beta2"),
          ref_ovl = TRUE)
 
-MCMCplot(samples_jags,
-         params = c("mu.beta0","beta1", "beta2","beta3",
-                    "mu.alpha0","alpha1","alpha2","alpha3",
-                    "scales_beta1","scales_beta2"),
-         ref_ovl = TRUE)
+#MCMCplot(samples_jags,
+#         params = c("mu.beta0","beta1", "beta2","beta3",
+#                    "mu.alpha0","alpha1","alpha2","alpha3",
+#                    "scales_beta1","scales_beta2"),
+#         ref_ovl = TRUE)
          
 #TODO:
 summary(samples_jags)
