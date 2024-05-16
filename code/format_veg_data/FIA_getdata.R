b1c406162a0f7e747e153a4ea80eb40b12046ddf
Bruna Amaral <74007922+br-amaral@users.noreply.github.com>
2024-05-16 14:12:17 -0400

    chenge conetticut park county

--- code/format_veg_data/FIA_getdata.R
+++ code/format_veg_data/FIA_getdata.R
@@ -42,7 +42,7 @@ park_county <- matrix(c(
   'SAIR', 'Essex County', 'Massachusetts',
   'SARA', 'Saratoga County', 'New York',
   'VAMA', 'Dutchess County', 'New York',
-  'WEFA', 'Fairfield County', 'Connecticut'), 
+  'WEFA', 'Western Connecticut Planning Region', 'Connecticut'), # used to be 'Fairfield County'
   ncol = 3, byrow = T) %>% 
   as_tibble()
 colnames(park_county) <- c("park", "county", "state")
