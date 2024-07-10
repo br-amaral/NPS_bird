8c716ab0c9a75b243d15042229013412ee5b91be
Bruna Amaral <74007922+br-amaral@users.noreply.github.com>
2024-07-10 14:46:41 -0400

    table and loop for running sps

--- code/fit_model/multiple_single_sps.R
+++ code/fit_model/multiple_single_sps.R
@@ -1,39 +1,68 @@
-# *********************************************************************************
-# -------------------------------   Amazing Title   -------------------------------
-# *********************************************************************************
-# Code to ...
+#? *********************************************************************************
+#? -------------------------------   Amazing Title   -------------------------------
+#? *********************************************************************************
 #
-# Source ---------------------------------------------
+#! Code to ...
+#
+#! Source ---------------------------------------------
+#           - code/fit_model/back2d_covs_scales_3.R:
+#!              - Input:
+#                   - data/y_dat8.rds: tibble with bird data
+#                   - data/X.rds: tibble with covariate data
+#                   - data/out/nsite_pk.rds: vector with number of sites in each park
+#                   - data/src/key_park.rds: vector of all parks being analyzed:
+#!              - Output: 
+#                   - data/model_res/jags_res_{sps}_{park}_run{run_number}.rds: file with result of jags model
+#
+#! Input ----------------------------------------------
 #           - :
 #           - :
 #
-# Input ----------------------------------------------
+#! Output ----------------------------------------------
 #           - :
 #           - :
-#
-# Output ----------------------------------------------
-#           - :
-#           - :
-#
+
+# Print script file name
+context <- "multiple_single_sps.R" #rstudioapi::getSourceEditorContext()
+cat("\n", "\n", "\n", 
+    'Current script:', context, #basename(context[[2]]), 
+    "\n", "\n", "\n", "\n")
+
+#! Package library and versions -------------------------
+#  Created a library repo?
+#  (  )yes  (  )no
+#  renv::init()
+
+# Load an existing library?
+#  renv::status()
+#  renv::restore()
+
+# Installed new packages?
+#  renv::snapshot()
+#options(repos = c(CRAN = "https://cloud.r-project.org/"))
+#update.packages("freshr")
 # detach packages and clear workspace
-if(!require(freshr)){install.packages('freshr')}
-freshr::freshr()
-#
-# Load packages ---------------------------------------
-library(conflicted)
+#if(!require(freshr)){install.packages('freshr')}
+#freshr::freshr()
+
+#! Load packages ---------------------------------------
+#library(conflicted)
 library(tidyverse)
 library(glue)
-#
-conflicts_prefer(dplyr::select)
-conflicts_prefer(dplyr::filter)
+
+#conflicts_prefer(dplyr::select)
+#conflicts_prefer(dplyr::filter)
 # conflicts_prefer(scales::alpha)
-#
-# Make functions --------------------------------------
+
+#! Make functions --------------------------------------
 colanmes <- colnames
 lenght <- length
 `%!in%` <- Negate(`%in%`)
-#
-# Source code -----------------------------------------
+
+#! Source code and Import data -----------------------------------------
+## file paths
+
+## read files
 #sps_list <- read_rds("data/src/guilds.rds")  %>% 
 #                filter(Response_Guild == "InteriorForestObligate") %>% 
 #                select(AOU_Code) %>% 
@@ -41,20 +70,25 @@ lenght <- length
 #                pull()
 
 #sps_list <- sps_list[-1]
-sps_list <- "RBWO" # sps_list[-4]
+sps_list1 <- c("GCFL", "AMGO", "DOWO", "NOCA", "SCTA", "SOSP", "GRCA", "RBWO", "COYE", "WOTH", "RWBL",
+                "WBNU", "BTNW", "EAWP", "BCCH", "BLJA", "TUTI", "AMRO", "REVI", "OVEN", "BTBW", "YBSA", 
+                "BOBO", "YRWA", "PIWA", "CEDW", "CHSP", "NOFL", "HAWO", "BRCR", "RBGR", "DEJU", "AMCR", 
+                "BAOR", "RBNU", "BHVI", "GCKI", "EATO", "FISP", "HETH", "VEER", "MODO", "BLBW")
 
-#c("GCFL", "AMGO", "DOWO", "NOCA", "SCTA", "SOSP", "GRCA", "RBWO", "COYE", "WOTH", "RWBL",
-#              "WBNU", "BTNW", "EAWP", "BCCH", "BLJA", "TUTI", "AMRO", "REVI", "OVEN", "BTBW", "YBSA", 
-#              "BOBO", "YRWA", "PIWA", "CEDW", "CHSP", "NOFL", "HAWO", "BRCR", "RBGR", "DEJU", "AMCR", 
-#              "BAOR", "RBNU", "BHVI", "GCKI", "EATO", "FISP", "HETH", "VEER", "MODO", "BLBW")
+yearbo1 <- c('yes', 'no')
 
-yearbo <- "yes"
+for_stage1 <- c('late', 'mature', 'pole')
 
-# parks <- 
+master_tab <- expand_grid(sps_list1, yearbo1, for_stage1) %>% 
+                mutate(res_name = glue("{sps_list1}_b0{yearbo1}_{for_stage1}"))
 
-for (i in 1:length(sps_list)){
-    sps_loop <- sps_list[i]
-    print(i)
+colnames(master_tab) <- c("sps_list", "yearbo", "for_stage", "res_name")
+## BTNW not working
+for (i in 151:nrow(master_tab)){
+    sps_loop <- master_tab[i,1] %>% pull()
+    yearbo <- master_tab[i,2] %>% pull()
+    for_stage <- master_tab[i,3] %>% pull()
+    sps_loop2 <- master_tab[i,4] %>% pull()
+    print(sps_loop2)
     source("code/fit_model/back2d_covs_scales_3.R")
 }
-
