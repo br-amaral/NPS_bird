8a5c572e52504a2b198e6b99910ccc0c2f1cb06a
Bruna Amaral <74007922+br-amaral@users.noreply.github.com>
2024-07-22 09:25:06 -0400

    make things simples: remove some species and forest age

--- code/fit_model/multiple_single_sps.R
+++ code/fit_model/multiple_single_sps.R
@@ -69,26 +69,24 @@ lenght <- length
 #                distinct() %>% 
 #                pull()
 
+# removing forest stage, species, 
 #sps_list <- sps_list[-1]
-sps_list1 <- c("GCFL", "AMGO", "DOWO", "NOCA", "SCTA", "SOSP", "GRCA", "RBWO", "COYE", "WOTH", "RWBL",
-                "WBNU", "BTNW", "EAWP", "BCCH", "BLJA", "TUTI", "AMRO", "REVI", "OVEN", "BTBW", "YBSA", 
-                "BOBO", "YRWA", "PIWA", "CEDW", "CHSP", "NOFL", "HAWO", "BRCR", "RBGR", "DEJU", "AMCR", 
-                "BAOR", "RBNU", "BHVI", "GCKI", "EATO", "FISP", "HETH", "VEER", "MODO", "BLBW")
+sps_list1 <- c("GCFL", "AMGO", "DOWO", "RBWO", "COYE")
 
-yearbo1 <- c('yes', 'no')
+yearbo1 <- c('yes')
 
-for_stage1 <- c('late', 'mature', 'pole')
+#for_stage1 <- c('late', 'mature', 'pole')
 
-master_tab <- expand_grid(sps_list1, yearbo1, for_stage1) %>% 
-                mutate(res_name = glue("{sps_list1}_b0{yearbo1}_{for_stage1}"))
+master_tab <- expand_grid(sps_list1, yearbo1) %>% 
+                mutate(res_name = glue("{sps_list1}_b0{yearbo1}"))
 
-colnames(master_tab) <- c("sps_list", "yearbo", "for_stage", "res_name")
+colnames(master_tab) <- c("sps_list", "yearbo", "res_name")
 ## BTNW not working
-for (i in 145:nrow(master_tab)){
+for (i in 1:nrow(master_tab)){
     sps_loop <- master_tab[i,1] %>% pull()
     yearbo <- master_tab[i,2] %>% pull()
-    for_stage <- master_tab[i,3] %>% pull()
-    sps_loop2 <- master_tab[i,4] %>% pull()
+    # for_stage <- master_tab[i,3] %>% pull()
+    sps_loop2 <- master_tab[i,3] %>% pull()
     print(sps_loop2)
     source("code/fit_model/back2d_covs_scales_3.R")
 }
