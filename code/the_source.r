efd3b0c5d9b1ac6ee694a9cca4700b479545efc3
Bruna Amaral <amaral.bruuna@gmail.com>
2024-10-20 18:50:34 -0400

    run all data generation and analysis

---
+++ code/the_source.r
@@ -0,0 +1,13 @@
+source("code/format_veg_data/get_site_data_forgroups.R")
+source("code/format_veg_data/get_park_data.R")
+source("code/format_veg_data/local_diversity.R")
+source("code/format_veg_data/FIA_getdata.R")
+source("code/format_veg_data/get_coun_data.R")
+source("code/format_bird_data/2_create_data_files.R")
+source("code/format_bird_data/2_create_data_files_yr.R")
+source("code/fit_model/multiple_single_sps_spscov.R")
+source("code/fit_model/multiple_single_sps_spscov_yr.R")
+
+# source("code/")
+# source("code/")
+# source("code/")
