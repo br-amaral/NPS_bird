12821e5bd2bdfb8cf6ac088771c82fcb69da02e9
Bruna Amaral <amaral.bruuna@gmail.com>
2024-10-20 21:44:54 -0400

    add a source file

--- code/the_source.r
+++ code/the_source.r
@@ -1,13 +1,19 @@
 source("code/format_veg_data/get_site_data_forgroups.R")
 source("code/format_veg_data/get_park_data.R")
 source("code/format_veg_data/local_diversity.R")
-source("code/format_veg_data/FIA_getdata.R")
+#source("code/format_veg_data/FIA_getdata.R")
 source("code/format_veg_data/get_coun_data.R")
+
 source("code/format_bird_data/2_create_data_files.R")
 source("code/format_bird_data/2_create_data_files_yr.R")
+source("code/format_bird_data/check_sps_data.r.R")
+
 source("code/fit_model/multiple_single_sps_spscov.R")
 source("code/fit_model/multiple_single_sps_spscov_yr.R")
 
 # source("code/")
 # source("code/")
 # source("code/")
+
+sbatch sbatch/nps_singlesps_parks_spscov.sb 
+sbatch sbatch/nps_singlesps_parks_spscov_yr.sb 
