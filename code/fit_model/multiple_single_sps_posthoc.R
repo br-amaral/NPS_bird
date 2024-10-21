680377a027129d71812b2bb862e390276aaabefa
Bruna Amaral <74007922+br-amaral@users.noreply.github.com>
2024-10-21 19:56:47 -0400

    run post hoc analysis with selected betas and scales

---
+++ code/fit_model/multiple_single_sps_posthoc.R
@@ -0,0 +1,88 @@
+#? *********************************************************************************
+#? ---------------------------   multiple_single_sps.R   ---------------------------
+#? *********************************************************************************
+#
+#! Code to ...
+#
+#! Source ---------------------------------------------
+#           - code/fit_model/back2d_covs_scales_2min.R:
+#!              - Input:
+#                   - data/y_dat8.rds: tibble with bird data
+#                   - data/X.rds: tibble with covariate data
+#                   - data/out/nsite_pk.rds: vector with number of sites in each park
+#                   - data/src/key_park.rds: vector of all parks being analyzed:
+#!              - Output: 
+#                   - data/model_res/jags_res_{sps}_{park}_run{run_number}.rds: file with result of jags model
+#
+#! Input ----------------------------------------------
+#           - :
+#           - :
+#
+#! Output ----------------------------------------------
+#           - :
+#           - :
+
+# Print script file name
+context <- "multiple_single_sps_spscovs.R" #rstudioapi::getSourceEditorContext()
+cat("\n", "\n", "\n", 
+    'Current script:', context, #basename(context[[2]]), 
+    "\n", "\n", "\n", "\n")
+
+#! Package library and versions -------------------------
+#  Created a library repo?
+#  (  )yes  (  )no
+#  renv::init()
+
+# Load an existing library?
+#  renv::status()
+#  renv::restore()
+
+# Installed new packages?
+#  renv::snapshot()
+#options(repos = c(CRAN = "https://cloud.r-project.org/"))
+#update.packages("freshr")
+# detach packages and clear workspace
+#if(!require(freshr)){install.packages('freshr')}
+freshr::freshr()
+
+#! Load packages ---------------------------------------
+#library(conflicted)
+library(tidyverse)
+library(glue)
+
+#conflicts_prefer(dplyr::select)
+#conflicts_prefer(dplyr::filter)
+# conflicts_prefer(scales::alpha)
+
+#! Make functions --------------------------------------
+colanmes <- colnames
+lenght <- length
+`%!in%` <- Negate(`%in%`)
+
+#! MCMC settings ---------------------------------------
+niterations <- 40000
+nburnin <- 20000
+nchains <- 5
+nthin <- 5
+
+# niterations <- 10
+# nburnin <- 5
+# nchains <- 1
+# nthin <- 1
+
+#! Source code and Import data -----------------------------------------
+## file paths
+
+## read files
+master_tab <- read_csv("data/out/post_hoc_ana.csv")
+
+for(i in 1:nrow(master_tab)){
+    (sps_loop <- master_tab$sps[i])
+    file_name <-  master_tab$file[i]
+    source("code/fit_model/post_hoc.R")
+
+}
+
+cat(paste('\n ************************************** \n \n \n 
+        ---------------- all DONE Lol ----------------', 
+        '\n\n \n ************************************** \n'))
