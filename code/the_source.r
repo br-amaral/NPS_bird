2dea810a7448f06a6476b01d594dbc5b459d5c29
Bruna Amaral <amaral.bruuna@gmail.com>
2025-02-10 16:20:22 -0500

    only run model cause formatting data is getting problems with R on hpcc

--- code/the_source.r
+++ code/the_source.r
@@ -1,11 +1,11 @@
-source("code/format_veg_data/get_site_data_forgroups.R")
-source("code/format_veg_data/get_park_data.R")
-source("code/format_veg_data/local_diversity.R")
-source("code/format_veg_data/FIA_getdata.R")
-source("code/format_veg_data/get_coun_data.R")
+# source("code/format_veg_data/get_site_data_forgroups.R")
+# source("code/format_veg_data/get_park_data.R")
+# source("code/format_veg_data/local_diversity.R")
+# source("code/format_veg_data/FIA_getdata.R")
+# source("code/format_veg_data/get_coun_data.R")
 
-source("code/format_bird_data/2_create_data_files.R")
-source("code/format_bird_data/check_sps_data.r.R")
+# source("code/format_bird_data/2_create_data_files.R")
+# source("code/format_bird_data/check_sps_data.r.R")
 
 source("code/fit_model/multiple_single_sps_spscov.R")
 
