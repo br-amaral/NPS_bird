bff9735a91e999aca2067b02cae9399c14535205
Bruna Amaral <amaral.bruuna@gmail.com>
2025-02-24 15:01:13 -0500

    plot correlations between scales when more than one is selected

--- code/fit_model/plots.r
+++ code/fit_model/plots.r
@@ -7,7 +7,7 @@ hg <- httpgd::hgd()
 # detach packages and clear workspace
 freshr::freshr()
 #
-# Load packages -----------------------------------------------------
+# Load packages -------------------------------------------------------------------
 library(conflicted)
 library(tidyverse)
 library(glue)
@@ -18,12 +18,12 @@ conflicts_prefer(dplyr::select)
 conflicts_prefer(dplyr::filter)
 # conflicts_prefer(scales::alpha)
 #
-# Make functions -----------------------------------------------------
+# Make functions ------------------------------------------------------------------
 colanmes <- colnames
 lenght <- length
 `%!in%` <- Negate(`%in%`)
 
-#! Import data -------------------------------------------------------
+#! Import data --------------------------------------------------------------------
 ## file paths and read files
 # when loading the model results, get the most updated file?
 file_name <- "2025_02_22_BHVI_parks_30000its_2min_spscov_run1"
@@ -34,14 +34,14 @@ samples_jags <- read_rds(glue("data/model_res/{file_name}.rds"))
 scales_names <- grep("^scales_", colnames(samples_jags[[1]]), value = TRUE)
 all_params <- c("mu.alpha0", "mu.beta0", "beta", "alpha", scales_names)
 
-#! Par estimates ------------------------------------------------------
+#! Par estimates ------------------------------------------------------------------
 par(mfrow = c(1,1))
 MCMCplot(samples_jags,
          params = all_params,
          main = file_name,
          ref_ovl = TRUE)
 
-#! Traceplots ---------------------------------------------------------
+#! Traceplots ---------------------------------------------------------------------
 MCMCtrace(samples_jags,
           params = all_params,
           #main = file_name,
@@ -51,12 +51,12 @@ MCMCtrace(samples_jags,
           Rhat = TRUE,
           n.eff = TRUE)
 
-#! Summary ------------------------------------------------------------
+#! Summary ------------------------------------------------------------------------
 MCMCsummary(samples_jags,
             params = all_params,
             round = 2)
 
-#! get beta parameters and selected scales ----------------------------
+#! get beta parameters and selected scales ----------------------------------------
 # beta parameters that the 50 percent CI does not include 0
 betas <- tidybayes::get_variables(samples_jags)
 n_betas1 <- sub("\\[.*", "", betas) 
@@ -71,7 +71,6 @@ beta_key <- tibble(
   sca2 = as.numeric(NA),
   sca3 = as.numeric(NA)
 )
-
 
 for(ii in 1:n_betas) {
 # betas
@@ -106,4 +105,28 @@ for(ii in 1:n_betas) {
 
 }
 
+# check how similar scales are if more than one was selected
+two_sca <- beta_key %>% 
+  filter(grepl("_", sca_sel))
+file_name_data <- glue("data/ana_file/{substr(file_name, 1, 10)}_data_{substr(file_name, 12, 15)}_parks.rds")
+jags_data <- read_rds(file_name_data)
+str(jags_data)
+
+dat_sca <- cbind(jags_data$X52[,2],
+                 jags_data$X52[,1],
+                 jags_data$y[,2]) %>% 
+                 as_tibble() %>% 
+                 rename(cov1 = V1, cov2 = V2, park = V3)  %>% 
+                 mutate(cov1 = as.numeric(cov1),
+                        cov2 = as.numeric(cov2),
+                        park = as.factor(park))
+
+ggplot(dat_sca) +
+    geom_point(aes(x = cov1, y = cov2, 
+                   colour = park),
+               alpha = 0.3) + 
+    theme_bw() + ggtitle(glue("correlation = {round(cor(dat_sca[,1], dat_sca[,2], method = 'spearman'),2)}"))
+
+# save beta and scale selection values
 write_rds(beta_key, file = glue("data/model_res/{file_name}_SCA_SEL_PARS.rds"))
+
