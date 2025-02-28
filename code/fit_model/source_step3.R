1cb94dc12d2650ca550f71a2cef0b4dd8254d77f
Bruna Amaral <amaral.bruuna@gmail.com>
2025-02-28 16:53:52 -0500

    change s[pecies and iteratrion to run step 2 analysis of species that are already ready

--- code/fit_model/source_step3.R
+++ code/fit_model/source_step3.R
@@ -42,14 +42,17 @@ MOD_RES_PATH <- "results_tracker.xlsx"
 ## read files
 mod_specs <- read_excel(MOD_RES_PATH, sheet = "key") %>%
                 filter(Step == 1) %>% 
-                select(OUTPUT_FILE)
+                filter(SPS %in% c("HETH", "OVEN", "VEER", "BTBW")) %>% 
+                select(OUTPUT_FILE, SPS)
 
 nchains <- 8
 niterations <- 30000
 nburnin <- 20000
 nthin <- 5
 
-for(jj in 6:nrow(mod_specs)){
+for(jj in 1:nrow(mod_specs)){
+
+    if(mod_specs$OUTPUT_FILE[jj] == "BTBW"){nchains <- 8;niterations <- 50000; nburnin <- 30000;nthin <- 5}else{nchains <- 8;niterations <- 30000; nburnin <- 20000;nthin <- 5}
 
     file_name <- mod_specs$OUTPUT_FILE[jj]
     source("code/fit_model/step2_analysis.R")
