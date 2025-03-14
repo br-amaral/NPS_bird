611d834fa6dab0aaa1a35107c518826fa32cd5c5
Bruna Amaral <amaral.bruuna@gmail.com>
2025-03-14 13:30:56 -0400

    correct dates, upload only necessary files, connect analysis with model_sps_key, better meta data file, and add test setting

--- code/fit_model/step2_analysis.R
+++ code/fit_model/step2_analysis.R
@@ -9,7 +9,7 @@
 #           - :
 #
 # Input ----------------------------------------------
-#           - data/model_res/{date_out}_{fil_nam}_{park_name}_{niterations}its_2min_spscov_yr_run{x}.rds
+#           - data/model_res/{date_step1}_{fil_nam}_{park_name}_{niterations}its_2min_spscov_yr_run{x}.rds
 #           - :
 #
 # Output ----------------------------------------------
@@ -41,91 +41,65 @@ lenght <- length
 #! Moving this row to a source file for multiple species
 # file_name <- '2025_02_24_HAWO_parks_30000its_2min_spscov_run1'
 
-(sps <- substr(file_name, 12, 15))
-(date_out <- substr(file_name, 1,  10))
+(sps <- sps_loop)
 system_time1 <- Sys.time()
+(date_step2 <- glue("{substr(system_time1, 1,4)}_{substr(system_time1, 6,7)}_{substr(system_time1, 9,10)}"))
 script_name <- "step2_analysis.R"
 
 ## file paths
-# selected parameters
-PAR_MOD_PATH <- glue('data/model_res/{file_name}_SCA_SEL_PARS.rds')
-
-# model results
-MODEL_RES_PATH <- glue("data/model_res/{file_name}.rds")
-
-# model data
-MODEL_DATA_PATH <- glue('data/ana_file/{date_out}_data_{sps}_parks.rds') 
-
-# meta data 
-META_DATA_PATH <- glue('data/ana_file/{date_out}_metadata_{sps}_parks.txt')
+# data
+SPS_DATA_PATH <- glue('data/ana_file/{date_step1}_data_{sps}_parks.rds') 
 
 # inital values (z)
-Z_DATA_PATH <- glue("data/ana_file/{date_out}_data_{sps}_Z.rds")
+Z_DATA_PATH <- glue("data/ana_file/{date_step1}_data_{sps}_Z.rds")
 
-#! TODO: change dates so the new file has a new date, not the old one
+#
+
+#! TODO: change dates so the new file has a new date, not the old one   
 
 ## read files
-print(meta_data <- readr::read_lines(META_DATA_PATH))
-samples_jags <- read_rds(MODEL_RES_PATH)
-jags_data <- read_rds(MODEL_DATA_PATH) 
-pars_sca_mod <- read_rds(PAR_MOD_PATH)
-
-# get X objects being used
-pars_mod_name <- substr(meta_data[7], 15, nchar(meta_data[7]))
-pars_mod_name2 <- gsub(" ", "", pars_mod_name)
-split_vector <- strsplit(pars_mod_name2, "_")
-pars_name_vec <- unlist(split_vector)
-
-(pars_mod <- pars_sca_mod  %>% 
-              mutate(X = pars_name_vec) %>% 
-              filter(overlap0 == "no") %>% 
-              mutate(betas = sub("(.{4})(.*)", "\\1[\\2", betas)) %>% 
-              mutate(betas = sub("(.{6})(.*)", "\\1]\\2", betas)) %>% 
-              mutate(b_sca_numb = ifelse(grepl("_", sca_sel), 2, 1),
-                     b_sca_numb2 = b_sca_numb) %>% 
-              expandRows(.,"b_sca_numb2")) 
-
-sca_mod <- pars_sca_mod  %>% 
-              filter(overlap0 == "no") %>% 
-              select(sca_sel) %>% 
-              pull()
-sca_mod_split <- sca_mod %>% strsplit("_") %>% unlist()  %>% as.numeric()
-pars_mod$uni_sca <- sca_mod_split
-
+jags_data <- read_rds(SPS_DATA_PATH) 
 z_mod <- read_rds(Z_DATA_PATH)$Zst2
 
-# get parameter names
-scales_names <- grep("^scales_", colnames(samples_jags[[1]]), value = TRUE)
-(all_params <- c("mu.alpha0", "mu.beta0", "beta", "alpha", scales_names))
+#! Get only parameters and scales that are relevant --------------------------------------------
+cov_key2 <- cov_key[,which(cov_key != 0)] %>% colnames()
+cov_key2 <- cov_key2[-1]
 
-n_bs <- length(pars_name_vec) + 1
-n_bs_new <- c(length(unique(pars_mod$betas)),nrow(pars_mod))
-beta_numbs <- glue("beta[{seq(1,n_bs-1,1)}]")
+cov_key2_numb <- which(cov_key != 0) - 1
+cov_key2_numb <- cov_key2_numb[-1]
 
-par_key <- as_tibble(cbind(c("X1", "X2", "X3", "X4", "X51", "X52", "X53", "X6", "X7"),
-                           c("BA", "DEN", "SHR", "DIV", "EAR", "MID", "LAT", "CAN", "DEB"))) %>% 
+# get X objects being used
+pars_sca_mod <- cbind(cov_key2, cov_key2_numb, scales_loop) %>% 
+                  as_tibble() %>% 
+                  rename(cov_name = cov_key2, 
+                         cov_numb = cov_key2_numb,
+                         scal = scales_loop) %>% 
+                  mutate(cov_numb = as.numeric(cov_numb),
+                         scal = as.numeric(scal),
+                         X = cov_name)
+
+# remove n_bs <- nrow(pars_sca_mod) + 1
+n_bs_new <- nrow(pars_sca_mod) + 1
+# remove beta_numbs <- glue("beta[{seq(1,n_bs-1,1)}]")
+
+par_key <- as_tibble(cbind(c("X1", "X2", "X3", "X4", 
+                             "X51", "X52", "X53", "X6", "X7"),
+                           c("BA", "DEN", "SHR", "DIV", 
+                             "EAR", "MID", "LAT", "CAN", "DEB"))) %>% 
                     rename(xobj = V1,
                            X = V2)
 
-pars_mod <- left_join(pars_mod, par_key, by = "X")
-
-#! Get only parameters and scales that are relevant --------------------------------------------
-## quick check
-MCMCsummary(samples_jags,
-            params = all_params,
-            round = 2)
+pars_mod <- left_join(pars_sca_mod, par_key, by = "X")
 
 #! Run the step 2 model ------------------------------------------------------------------------
-covs_names <- paste(pars_mod$X, collapse = "_")
-sca_names <- paste(pars_mod$uni_sca, collapse = "_")
+(covs_names <- paste(pars_mod$X, collapse = "_"))
+(sca_names <- paste(pars_mod$scal, collapse = "_"))
 
 params <- c("beta0", "beta", "alpha0", "alpha", 
             "mu.beta0", "tau.beta0", "mu.alpha0", "tau.alpha0") %>% # Z, psi
           as.character()
 
 n_as <- 3
-
-n_bs_new <- n_bs_new[2] + 1
 
 inits <- function() {
     list(
@@ -137,8 +111,8 @@ inits <- function() {
 }
 
 # Define the model file and the output file name
-model_file <- glue("models/mod_1_vector_spscov_{sps}_step2.txt")
-mod_name   <- glue("data/ana_file/{date_out}_mod_{sps}_step2.txt") %>% as.character()
+model_file <- mod_name_loop
+mod_name   <- glue("data/ana_file/{sps}_step{step_numb}_model_{date_step2}.txt") %>% as.character()
 
 # Read the content of the model file
 mod_content <- readLines(model_file)
@@ -147,7 +121,7 @@ mod_content <- readLines(model_file)
 mod_string <- paste(mod_content, collapse = "\n")
 
 # Write the content to the output file
-writeLines(mod_string, mod_name)
+if(test == FALSE){writeLines(mod_string, mod_name)}
 
 # get the right data for the model - remove the covariates that are no longer in use
 old_pars <- names(jags_data)
@@ -160,6 +134,14 @@ jags_data2$n_bs <- n_bs_new
 names(jags_data2)
 str(jags_data2)
 
+if(test == TRUE){
+  nchains <- 1
+  niterations <- 6
+  nburnin <- 1
+  nthin <- 1
+  print("test with 5 iterations")
+}
+
 ## initialize JAGS
 cat("\n\n\n running jags \n\n\n\n")
 
@@ -171,11 +153,13 @@ samples_jags <- jags(data = jags_data2,
                       n.adapt = max(500, ceiling(.1 * niterations)),
                       n.iter = niterations,
                       n.burnin = nburnin,
-                      n.thin = nthin)
+                      n.thin = nthin,
+                      parallel = TRUE,
+                      n.cores = nchains)
 
 cat("\n\n\n model is done!!! \n\n\n\n")
 
-file_name <- glue("{date_out}_{sps}_{niterations}its_2min_spscov_step2_")
+file_name <- glue("{sps}_step{step_numb}_output_{date_step2}")
 
 file_name2 <- paste0(file_name, 'run',
                       length(list.files(path = file.path(getwd(),"data/model_res/"),
@@ -188,12 +172,17 @@ if (!file.exists(folder_path)) {
   dir.create(folder_path, recursive = TRUE)
 }
 
-write_rds(samples_jags,
-          file = glue('data/model_res/{file_name2}.rds')
-          )
+if(test == FALSE) {
+  write_rds(samples_jags,
+            file = glue('data/model_res/{file_name2}.rds')
+            )
+}
 
-# Get covariate names
-covs_names2 <- paste(unique(pars_mod$X), collapse = "_")
+if(niterations > 10000) {
+  write_rds(samples_jags,
+            file = glue('data/model_res/{file_name2}.rds')
+            )
+}
 
 system_time2 <- Sys.time()
 if(as.numeric(system_time2 - system_time1) < 60) {
@@ -212,7 +201,8 @@ paste('\n ************************************** \n \n \n ---------------- DONE 
       'Output File Name = ', glue('{file_name2}.rds'), '\n', 
       'Script = ', script_name, '\n', 
       'Species =', sps, '\n',
-      'Covariates =', covs_names2, '\n',
+      'Covariates =', covs_names, '\n',
+      'Scales =', sca_names, '\n',
       'Iterations =', niterations, '\n',
       'Run number =', str_split(file_name2, 'run', simplify = TRUE)[2], '\n',
       'Started running on =', system_time1, '\n',
@@ -222,24 +212,39 @@ paste('\n ************************************** \n \n \n ---------------- DONE 
       cat()
 
 
-meta_name <- file(glue("data/ana_file/{date_out}_metadata_{sps}_step2.txt"))
-writeLines(paste(
+meta_name <- file(glue("data/ana_file/{sps}_step{step_numb}_metadata_{date_step2}.txt"))
+if(test == FALSE){
+    writeLines(paste(
 
-                ' Results File Name =', glue('{file_name2}.rds'), '\n', 
-                'Data File Name =', glue("data/ana_file/{date_out}_data_{sps}_step2.rds"), '\n', 
-                'Script =', script_name, '\n',
-                'Model file =', glue("{mod_name}"), '\n',
-                'Species =', sps, '\n',
-                'Number of betas =', n_bs_new, '\n',
-                'Covariates =', covs_names2, '\n',
-                'Scales =', sca_names, '\n',
-                'Iterations =', niterations, '\n',
-                'Chains =', nchains, '\n',
-                'Burn-in =', nburnin, '\n',
-                'Run number =', str_split(file_name2, 'run', simplify = TRUE)[2], '\n',
-                'Started running on =', system_time1, '\n',
-                'Stopped running on =', system_time2, '\n',
-                'Time it took =', time_it_took , unit_time), 
-          meta_name)
+                  'Species =', sps, '\n',
+                  'Step =', step_numb, '\n',
+                  'Date =', date_step2, '\n',
 
-close(meta_name)
+                  'Metadata File Name =', meta_name, '\n', 
+                  'Results File Name =', glue('{file_name2}.rds'), '\n', 
+                  'Model File Name =', glue("{mod_name}"), '\n',
+                  'Data File Name =', SPS_DATA_PATH, '\n', 
+                  'Z File Name =', Z_DATA_PATH, '\n', 
+
+                  'Covariates =', covs_names, '\n',
+                  'Scales =', sca_names, '\n',
+                                  
+                  'Script =', script_name, '\n',
+                  'Iterations =', niterations, '\n',
+                  'Chains =', nchains, '\n',
+                  'Burn-in =', nburnin, '\n',
+                  'Thinning =', nthin, '\n',
+
+                  'Run number =', str_split(file_name2, 'run', simplify = TRUE)[2], '\n',
+                  'Started running on =', system_time1, '\n',
+                  'Stopped running on =', system_time2, '\n',
+                  'Time it took =', time_it_took , unit_time), 
+
+            meta_name)
+
+  close(meta_name)
+}
+
+if(test == TRUE){
+  cat(glue("\n \n \n \n Test for {sps} and step {step_numb} done!  \n \n \n \n \n"))
+}
