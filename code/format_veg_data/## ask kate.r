767a74aca32085810d4e19d5e14edd2383b17615
Bruna Amaral <amaral.bruuna@gmail.com>
2025-09-13 16:14:01 -0400

    delete unused files

--- code/format_veg_data/## ask kate.r
+++
@@ -1,7 +0,0 @@
-## ask kate
-
-
-shrub <- joinMicroShrubData() %>%  
-            as_tibble() %>% 
-            filter(Shrub == 1) %>%         #should I include vines?
-            filter(is.na(shrub_avg_cov)) 
