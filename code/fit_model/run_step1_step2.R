e594bfc4e7e1b63a16576cc7cea8c10c350b516e
Bruna Amaral <74007922+br-amaral@users.noreply.github.com>
2025-10-07 14:23:37 -0400

    run for REVI!

--- code/fit_model/run_step1_step2.R
+++ code/fit_model/run_step1_step2.R
@@ -90,7 +90,7 @@ paste('\n ************************************* \n \n \n   Running Models:', '\n
       '  Started running on =', Sys.time(),  '\n \n \n',
       '**************************************') %>% cat()
 
- for (key_ite in 13:nrow(master_tab)){
+ for (key_ite in 12:12){
     # key_ite <- 1
     nburnin <- 30000
 
