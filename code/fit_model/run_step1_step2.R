c673e2f97829e94cb57039991afd2dfe8eea4fe0
Bruna Amaral <74007922+br-amaral@users.noreply.github.com>
2025-10-08 17:50:45 -0400

    not a test!

--- code/fit_model/run_step1_step2.R
+++ code/fit_model/run_step1_step2.R
@@ -40,7 +40,7 @@ freshr::freshr()
 # Installed new packages?
 #  renv::snapshot()
 
-test <- TRUE 
+test <- FALSE 
 interaction <- TRUE
 step_number_define <- 2
 if(substr(getwd(), 1, 3) == "/Us") {direc <- "local"} else {direc <- "hpc"}
