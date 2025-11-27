edf0ea513af27b8ebcca54b356a6d9eccf4eb96e
Bruna Amaral <74007922+br-amaral@users.noreply.github.com>
2025-11-27 15:00:16 -0500

    typo

--- code/fit_model/run_step1_step2.R
+++ code/fit_model/run_step1_step2.R
@@ -91,7 +91,7 @@ paste('\n ************************************* \n \n \n   Running Models:', '\n
       '  Started running on =', Sys.time(),  '\n \n \n',
       '**************************************') %>% cat()
 
-for(key_ite in 1:nrow(AOU_Code)){
+for(key_ite in 1:nrow(master_tab)){
     # key_ite <- 1
     #nburnin <- 30000
 
